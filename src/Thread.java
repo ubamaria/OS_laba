c389944e31f731cf62fae2dfbbb1719f88b90adf
Мария <uba2014@mail.ru>
2020-04-17 17:48:07 +0400

    laba2

--- src/Thread.java
+++ src/Thread.java
@@ -1,35 +1,31 @@
-public class Thread
-    {
-        private String name;
-        private int max_time;
-        private int currentTime;
-        private int requiredTime;
+public class Thread {
+	private String name;
+	private int max_time;
+	private int currentTime;
+	private int requiredTime;
 
-        public Thread(String thread, int requiredTime, int max_time)
-        {
-            this.max_time = max_time;
-            this.requiredTime = requiredTime;
-            this.currentTime = 0;
-            this.name = "thread " + thread;
-        }
-        public void execute()
-        {
-            currentTime++;
-            
-        }
-        public String getName()
-        {
-           return name;
-        }
+	public Thread(String thread, int requiredTime, int max_time) {
+		this.max_time = max_time;
+		this.requiredTime = requiredTime;
+		this.currentTime = 0;
+		this.name = "thread " + thread;
+	}
 
-        public boolean needTime()
-        {
-            return requiredTime > currentTime;
-        }
+	public void execute() {
+		currentTime++;
 
-        public boolean haveTime()
-        {
-            return max_time > currentTime;
-        }
-        
-    }
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public boolean needTime() {
+		return requiredTime > currentTime;
+	}
+
+	public boolean haveTime() {
+		return max_time > currentTime;
+	}
+
+}
